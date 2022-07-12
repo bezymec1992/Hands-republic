3e65a6bac849f0a794c671e558927285e9cc42d4
Vasiliy <bbezzymecc@gmail.com>
2022-07-12 13:33:19 +0300

    added full page

---
+++ components/MainSlot.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <div class="main-section">
+            <div class="container">
+                <div class=" flex-column-reverse flex-lg-row row">
+                    <div class="main__title-wrapp col-12 col-lg-6">
+                        <h1 class="h1 main__title">
+                            Growing your business
+                        </h1>
+                        <Button title="Our company" class="btn-dark" :has-icon="true" type="button" />
+                    </div>
+                    <div class="main-img col-12 col-lg-6">
+                        <picture >
+                            <source srcset="@/assets/imgs/main-mb.jpg" media="(max-width: 576px)">
+                            <img src="@/assets/imgs/mm.png" alt="MDN">
+                        </picture>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MainSlot",
+    }
+</script>
+
+<style lang="scss" scoped>
+.main-section {
+    background: #E5CE5F;
+    position: relative;
+    overflow: hidden;
+    @include media-breakpoint-down(lg) {
+        background: #F9ECA2;
+        
+    }
+    .main__title-wrapp {
+        z-index: 1;
+        margin-top: 240px;
+
+        .main__title {
+            padding-bottom: 40px;
+            @include media-breakpoint-down(md) {
+                font-size: 50px;
+                line-height: 60px;
+                max-width: 335px;
+            }
+        }
+        padding-bottom: 100px;
+        @include media-breakpoint-down(lg) {
+        margin-top: 40px;
+        }
+    }
+    .main-img {
+        margin-left: -350px;
+
+        @include media-breakpoint-down(lg) {
+            margin-left: -250px;
+        }
+        @include media-breakpoint-down(sm) {
+            margin-left: -50px;
+        }
+
+        &::before{
+        content: "";
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        width: 4rem;
+        height: 100%;
+        background: linear-gradient(180deg, #E5CE5F 0%, rgba(229, 206, 95, 0) 100%);
+}
+    }
+}
+    .h1 {
+        font-size: 100px;
+        line-height: 110px;
+        text-transform: uppercase;
+        width: 596px;
+        max-width: 100%;
+    }
+</style>
--- pages/ourCompany.vue
+++ pages/ourCompany.vue
@@ -1,24 +1,53 @@
 <template>
     <div>
+        <main-slot>
+
+        </main-slot>
+        
+        <InfoWrapper :title="info.title" :text1="info.text1" :text2="info.text2" :span1="info.span1" :span2="info.span2" :img="info.img" :imgMob="info.imgMob"/>
+
         <div class="core">
             <div class="container">
-                
-                    <h2 class="h2 core__title">
-                        Our core values
-                    </h2>
-                    <div class="core__items row d-flex justify-content-center">
-                        <p class="core__item col-12 col-xl-3">Empowering businesses</p>
-                        <p class="core__item col-12 col-xl-3">Customer obsessed</p>
-                        <p class="core__item col-12 col-xl-3">Embrace difference</p>
-                        <p class="core__item col-12 col-xl-3">Never stop improving</p>
-                    </div>
-                
+                <h2 class="h2 core__title">
+                    Our core values
+                </h2>
+                <div class="core__items row d-flex justify-content-center">
+                    <p class="core__item col-12 col-xl-3">Empowering businesses</p>
+                    <p class="core__item col-12 col-xl-3">Customer obsessed</p>
+                    <p class="core__item col-12 col-xl-3">Embrace difference</p>
+                    <p class="core__item col-12 col-xl-3">Never stop improving</p>
+                </div>
             </div>
         </div>
 
-    
-            
+        <div class="hands">
+            <div class="container">
+                <div class="hands__wrapper">
+                    <select name="cars" id="cars" class="select">
+                        <option value="volvo">Select hands you need...</option>
+                        <option value="volvso">Select hands you need...</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+   
         <quote-section :text="quote.text" :name="quote.name" :position="quote.position" :img="quote.img" :imgMob="quote.imgMob"/>
+
+        <div class="shaping">
+            <div class="container">
+                <div class="shaping__wrapper row justify-content-between">
+                    <h2 class="h2 shaping__title col-12 col-xl-6">
+                        The people shaping <br class="title-br"/> Hands Republic 
+                    </h2>
+                    <div class="shaping__text-wap col-12 col-xl-6">
+                        <p class="shaping__text">
+                            Our passionate executive and non-executive board members reflect the entrepreneurial spirit that brought about Heands Republic and remains at the heart of how our business is evolving.
+                        </p>
+                        <Button title="Find your team" class="btn-dark" :has-icon="true" type="button"/>
+                    </div>
+                </div>
+            </div>
+        </div>
             
         <div class="cards">
             <div class="container">
@@ -30,21 +59,33 @@
             </div>
         </div>
 
-
     </div>
 </template>
 <script>
+import MainSlot from "@/components/MainSlot";
+import InfoWrapper from "@/components/InfoWrapper";
 import QuoteSection from "@/components/QuoteSection";
 import Card from "@/components/Card";
 
 
 export default {
     components: {
+        MainSlot,
+        InfoWrapper,
         QuoteSection,
 		Card
 	},
      data() {
 		return {
+            info: {
+                title: "Our story",
+                text1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                text2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+                span1: "Pellentesque ac blandit leo, nec porta velit.",
+                span2: "Pellentesque ac blandit leo, nec porta velit.",
+                img: "storry-desk.jpg",
+                imgMob: "storry-mb.jpg"
+            },
             quote: {
                 text: "We are taking the lead in Europe to become the most trusted online enabler, and we realise this can only be done through sharing knowledge and ideas.",
                 name: "Jonas Dhaenens, ",
@@ -97,17 +138,62 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .hands {
+        padding-bottom: 100px;
+        .hands__wrapper {
+        max-width: 1200px;
+        min-height: 600px;
+        background: url("@/assets/imgs/hands-desk.png") no-repeat center;
+        background-size: cover;
+        position: relative;
+        margin: 0 auto;
+         @include media-breakpoint-down(md) {
+            background: url("@/assets/imgs/hands-mb.png") no-repeat center;
+            min-height: 350px;
+
+         }
+        .select {
+            position: absolute;
+            top: 52%;
+            left: 52%;
+            transform: translate(-52%, -50%);
+            width: 910px;
+            max-width: 100%;
+            height: 80px;
+            opacity: 0.3;
+            border: 1px solid #000000;
+            
+            
+
+            font-size: 30px;
+            line-height: 41px;
+            opacity: 0.5;
+            padding: 0 40px;
+            @include media-breakpoint-down(md) {
+                width: 450px;
+                height: 40px;
+                font-size: 16px;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+            }
+        }
+    }
+    }
     .core {
         max-width: 1200px;
+        padding: 70px 0;
         margin: 0 auto;
         .core__title {
             text-align: center;
             font-size: 100px;
             line-height: 120px;
             text-transform: uppercase;
+            margin-bottom: 40px;
             @include media-breakpoint-down(md) {
                 font-size: 50px;
                 line-height: 60px;
+                margin-bottom: 20px;
             }
         }
         .core__item:not(:last-child) {
@@ -143,6 +229,62 @@ export default {
             }
         }
     
+    }
+    .shaping {
+        padding-bottom: 200px;
+        @include media-breakpoint-down(lg) {
+            padding-bottom: 120px;
+        }
+        .row {
+            padding: 0 20px;
+        }
+        .shaping__text {
+            font-size: 24px;
+            line-height: 43px;
+            margin-bottom: 50px;
+            @include media-breakpoint-down(md) {
+                font-size: 20px;
+                line-height: 32px;
+                margin-bottom: 40px;
+            }
+        }
+        .shaping__title {
+            width: 568px;
+            max-width: 100%;
+            padding: 0;
+            @include media-breakpoint-down(xl) {
+                width: fit-content;
+                text-align: center;
+                .title-br {
+                    display: none;
+                }
+            }
+            @include media-breakpoint-down(md) {
+                text-align: left;
+                width: 320px;
+                margin-bottom: 0px;
+            }
+        }
+        .shaping__text-wap {
+            margin-top: 50px;
+            padding: 0;
+            @include media-breakpoint-down(xl) {
+                text-align: center
+            }
+            @include media-breakpoint-down(md) {
+                text-align: left;
+                margin-top: 30px;
+            }
+            .btn-dark {
+                @include media-breakpoint-down(xl) {
+                    margin: 0 auto;
+                }
+                @include media-breakpoint-down(md) {
+                    margin: 0;
+                }
+                
+            }
+        }
     }
     .cards {
         .row {
