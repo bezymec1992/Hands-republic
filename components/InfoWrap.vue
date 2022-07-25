0377963e52e5d92b7db7c5568e9235dfb374baa2
Andrii Biekasov <bekasovduet@gmail.com>
2022-07-25 09:42:46 +0100

    Info Wrap component changes

--- components/InfoWrap.vue
+++ components/InfoWrap.vue
@@ -2,7 +2,7 @@
   <div class="wrap-info">
     <div class="container">
       <div class="row g-0">
-        <div class="col-12 col-md-7">
+        <div class="col-12 col-xxl-7">
           <div class="wrap-info__img">
             <picture>
               <source
@@ -20,7 +20,7 @@
             </picture>
           </div>
         </div>
-        <div class="col-12 col-md-5 d-flex align-items-center">
+        <div class="col-12 col-xxl-5 d-flex align-items-center">
           <div class="wrap-info__wrapper">
             <h2 class="h2 wrap-info__title">
               {{ title }}
@@ -92,7 +92,9 @@ export default {
   overflow: hidden;
 
   &__img {
-    margin-left: -8rem;
+    margin-left: -3rem;
+    margin-right: -3rem;
+    margin-bottom: 3.3rem;
 
     @include media-breakpoint-down(sm) {
       margin-left: -2rem;
@@ -101,26 +103,19 @@ export default {
 
     @include media-breakpoint-down(md) {
       margin-right: -2.4rem;
-      margin-bottom: 3.3rem;
+      // margin-bottom: 3.3rem;
     }
 
     img {
       width: 100%;
       height: auto;
     }
-
-    // width: 100%;
-    // height: auto;
-    // margin-bottom: 3.2rem;
-    // @include media-breakpoint-up(md) {
-    //   margin-bottom: 0;
-    // }
   }
 
   &__title {
     margin-bottom: 1.6rem;
 
-    max-width: 35rem;
+    // max-width: 35rem;
     @include media-breakpoint-up(xxl) {
       margin-top: 0;
       margin-bottom: 3.2rem;
@@ -128,16 +123,14 @@ export default {
     }
   }
   &__wrapper {
-    @include media-breakpoint-up(md) {
-      padding-left: 2rem;
-    }
-    @include media-breakpoint-up(xl) {
+    // @include media-breakpoint-up(md) {
+    //   padding-left: 2rem;
+    // }
+    @include media-breakpoint-up(xxl) {
       padding-left: 8rem;
     }
   }
-  // &__text-holder {
-  //   max-width: 45rem;
-  // }
+
   &__text {
     font-size: 2rem;
     line-height: 1.6;
